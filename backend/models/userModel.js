074f3233ea763e80c3b6764c4db5e4288cd03c14
Agon Bllaca <agonbllaca21@hotmail.com>
2022-09-01 09:57:32 +0200

    Minor fix to user schema middleware

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -89,8 +89,6 @@ userSchema.pre("save", async function(next) {
 });
 
 userSchema.pre("save", function(next) {
-  if (!this.isModified("password") || this.isNew) return next();
-
   this.lastChangedDate = Date.now() - 1000;
   next();
 });
